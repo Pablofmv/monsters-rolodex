02c1b474f0b74786d459e50ab634ddf636cd277d
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-04-02 19:20:38 -0500

    Updates

---
+++ precache-manifest.ebf72de4ba928a92a7cb63e26377b586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655891718f347dfa0f864732274d4605",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "03acd5c8ca1caee45a1c",
+    "url": "/monsters-rolodex/static/css/main.258b5d43.chunk.css"
+  },
+  {
+    "revision": "00ccbf301e32b3d292fd",
+    "url": "/monsters-rolodex/static/js/2.29ffb1cf.chunk.js"
+  },
+  {
+    "revision": "5493ac8c9e221eeba3bec532634c6faf",
+    "url": "/monsters-rolodex/static/js/2.29ffb1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03acd5c8ca1caee45a1c",
+    "url": "/monsters-rolodex/static/js/main.0d63b00e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
