be088fdd1d40a76f155ec1d960d5c500d0bb7bc8
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-04-02 19:20:00 -0500

    deploy my react app

---
+++ build/precache-manifest.264560b19bf817fbde64da31c1851947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f81b374d37e1693ae58f3b36dad78e3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c9a39a7172b84fb0f5dd",
+    "url": "/monsters-rolodex/static/css/main.258b5d43.chunk.css"
+  },
+  {
+    "revision": "d6ca450a1f5c45f97814",
+    "url": "/monsters-rolodex/static/js/2.9fdab8b4.chunk.js"
+  },
+  {
+    "revision": "5493ac8c9e221eeba3bec532634c6faf",
+    "url": "/monsters-rolodex/static/js/2.9fdab8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a39a7172b84fb0f5dd",
+    "url": "/monsters-rolodex/static/js/main.f6da6c9b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
